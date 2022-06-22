b0f8ba9fe54241a505e5faac02ae82056c2126e1
deuxy21 <deuxy21@naver.com>
2022-06-22 21:33:09 +0900

    123

---
+++ src/main/java/com/lsm/sprintboot04/SprintBoot04Application.java
@@ -0,0 +1,13 @@
+package com.lsm.sprintboot04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBoot04Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprintBoot04Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/lsm/sprintboot04/SprintBoot04ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsm.sprintboot04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintBoot04ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
